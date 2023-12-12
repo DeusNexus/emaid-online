83eeeeff3051579c8cd1a43fc4f9cdb8cefc7b8f
DeusNexus <nextgenage@gmail.com>
2023-12-12 16:29:06 +0100

    Add build directory for website

---
+++ build/precache-manifest.5fd696b9d9dd9e6cf61785f811dabe20.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6ed55e121b0cd6cbbfc6",
+    "url": "/static/css/main.3caa52c7.chunk.css"
+  },
+  {
+    "revision": "6ed55e121b0cd6cbbfc6",
+    "url": "/static/js/main.08eff1fb.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "9d5ef475a6fd632102ba",
+    "url": "/static/js/2.1b42c62c.chunk.js"
+  },
+  {
+    "revision": "3230436c2dbd9efb40d6d7fd5e2ee055",
+    "url": "/index.html"
+  }
+];
