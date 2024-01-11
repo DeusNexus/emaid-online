8592f3342eddeee5b12b3276c1c7413ab45a07da
DeusNexus <nextgenage@gmail.com>
2024-01-11 21:30:49 +0100

    New Build

---
+++ build/precache-manifest.e45f695cea7ca7a57f16e9503b7c69e9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6541e8e6f9d8097c2bc7",
+    "url": "/static/css/main.d6d9ad51.chunk.css"
+  },
+  {
+    "revision": "6541e8e6f9d8097c2bc7",
+    "url": "/static/js/main.8b23e3be.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "9d5ef475a6fd632102ba",
+    "url": "/static/js/2.1b42c62c.chunk.js"
+  },
+  {
+    "revision": "592622241513b1f8d4c1cabccc5b5a91",
+    "url": "/index.html"
+  }
+];
